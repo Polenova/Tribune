2fd5b38042b733aaa2e8defaa903381b53eb8aee
Marina Polenova <m.v.polenova@gmail.com>
2020-12-18 11:46:51 +0100

    rename v1

--- app/src/main/java/ru/polenova/tribune/api/API.kt
+++ app/src/main/java/ru/polenova/tribune/api/API.kt
@@ -6,37 +6,37 @@ import ru.polenova.tribune.postModel.Post
 import ru.polenova.tribune.postModel.PostRequestDto
 
 interface API {
-    @POST("api/V1/authentication")
+    @POST("api/v1/authentication")
     suspend fun authenticate(@Body authRequestParams: AuthRequestParams): Response<Token>
 
-    @POST("api/V1/registration")
+    @POST("api/v1/registration")
     suspend fun register(@Body authRequestParams: AuthRequestParams): Response<Token>
 
-    @POST("api/V1/posts")
+    @POST("api/v1/posts")
     suspend fun createPost(@Body postRequestDto: PostRequestDto): Response<Void>
 
-    @GET("api/V1/posts")
+    @GET("api/v1/posts")
     suspend fun getPosts(): Response<List<Post>>
 
-    @GET("api/V1/posts/recent")
+    @GET("api/v1/posts/recent")
     suspend fun getRecent(): Response<List<Post>>
 
-    @GET("api/V1/posts/{idPost}/get-posts-after")
+    @GET("api/v1/posts/{idPost}/get-posts-after")
     suspend fun getPostsAfter(@Path("id") idPost: Long): Response<List<Post>>
 
-    @GET("api/V1/posts/{idPost}/get-posts-before")
+    @GET("api/v1/posts/{idPost}/get-posts-before")
     suspend fun getPostsBefore(@Path("id") idPost: Long): Response<List<Post>>
 
-    @POST("api/V1/posts/{idPost}/up")
+    @POST("api/v1/posts/{idPost}/up")
     suspend fun pressedPostUp(@Path("idUser") idPost: Long): Response<Post>
 
-    @DELETE("api/V1/posts/{idPost}/up")
+    @DELETE("api/v1/posts/{idPost}/up")
     suspend fun pressedPostUpRemove(@Path("idUser") idPost: Long): Response<Post>
 
-    @POST("api/V1/posts/{idPost}/down")
+    @POST("api/v1/posts/{idPost}/down")
     suspend fun pressedPostDown(@Path("idUser") idPost: Long): Response<Post>
 
-    @DELETE("api/V1/posts/{idPost}/down")
+    @DELETE("api/v1/posts/{idPost}/down")
     suspend fun pressedPostDownRemove(@Path("idUser") idPost: Long): Response<Post>
 
 }
